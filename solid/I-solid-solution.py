37b11e3ff306f14341626de51dcdbea51d0a79a0
nicoh182 <niconuel@hotmail.com>
2025-01-01 20:00:48 -0500

    Update I-solid-solution.py

--- solid/I-solid-solution.py
+++ solid/I-solid-solution.py
@@ -1,1 +1,21 @@
 # Here your solution
+from abc import ABC, abstractmethod
+
+class IPaymentGateway1(ABC):
+    @abstractmethod
+    def pay(self, amount: float) -> None:
+        pass
+
+class IPaymentGateway2(ABC):
+    @abstractmethod
+    def refund(self, amount: float) -> None:
+        pass
+
+    @abstractmethod
+    def handle_dispute(self, dispute_id: str) -> None:
+        pass
+
+class BasicGiftCard(IPaymentGateway1):
+    def pay(self, amount: float) -> None:
+        print(f"Gift card used to pay {amount}.")
+        

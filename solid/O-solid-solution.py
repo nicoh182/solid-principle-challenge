6a8445f727a2aec57be7579ece0718f17dd2233d
nicoh182 <niconuel@hotmail.com>
2025-01-01 19:59:33 -0500

    Update O-solid-solution.py

--- solid/O-solid-solution.py
+++ solid/O-solid-solution.py
@@ -1,1 +1,24 @@
 # Here your solution
+from abc import ABC, abstractmethod
+
+class PaymentMethod(ABC):
+    @abstractmethod
+    def fee(self, amount):
+        return 0
+
+class CreditCardPayment(PaymentMethod):
+    def fee(self, amount):
+        return amount * 0.03
+    
+class PayPalPayment(PaymentMethod):
+    def fee(self, amount):
+        return amount * 0.05
+
+class BankTransferPayment(PaymentMethod):
+    def fee(self, amount):
+        return 2.50
+
+class FeeCalculator:
+    @staticmethod
+    def calculate_fee(amount, payment_method: PaymentMethod):
+        return payment_method.fee(amount)

2693249b24693982baff9864441afe4a03d6af15
nicoh182 <niconuel@hotmail.com>
2025-01-01 20:01:13 -0500

    Update D-solid-solution.py

--- solid/D-solid-solution.py
+++ solid/D-solid-solution.py
@@ -1,1 +1,16 @@
 # Here your solution
+from abc import ABC, abstractmethod
+
+class IntefacePayService(ABC):
+    @abstractmethod
+    def pay(amount: float):
+        pass
+
+class PayPalService(IntefacePayService):
+    @staticmethod
+    def pay(amount: float):
+        print(f"Paying {amount} using PayPal...")
+
+class PaymentProcessor:
+    def process_payment(self, pay_service: IntefacePayService, amount: float):
+        pay_service.pay(amount)
